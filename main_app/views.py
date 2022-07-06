0122d16e4dbf69c1b494161c9d42b0b37d29da1b
ana kass <anakass@anasemoss-mbp.mynetworksettings.com>
2022-07-06 01:04:16 -0400

    added routes and templates

--- main_app/views.py
+++ main_app/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+# Add the following import
+# from django.http import HttpResponse
 
 # Create your views here.
+
+
+# Define the home view
+def home(request):
+    return render(request, 'home.html')
+
+
+def about(request):
+    return render(request, 'about.html')
