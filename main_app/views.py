8fe2605ccf298b90b8ca0635b25d89805f4e584b
Anasemos Kassahun <anasemosaka@gmail.com>
2022-07-06 16:53:29 -0400

    added routes and templates

--- main_app/views.py
+++ main_app/views.py
@@ -12,3 +12,25 @@ def home(request):
 
 def about(request):
     return render(request, 'about.html')
+
+
+# Add the Cat class & list and view function below the imports
+class Game:  # Note that parens are optional if not inheriting from another class
+    def __init__(self, title, genre, description, age):
+        self.title = title
+        self.genre = genre
+        self.description = description
+        self.age = age
+
+
+games = [
+    Game('Fifa 22', 'Sports', "Powered by Football and features groundbreaking new HyperMotion gameplay technology on PlayStation 5, Xbox Series X|S, and Stadia.", 1),
+    Game('Call of Duty Warzone', 'Shooter',
+         'free-to-play battle royale video game', 2),
+    Game('NBA 2K22', 'Sports', 'basketball simulation video game developed by Visual Concepts and published by 2K Sports, based on the National Basketball Association', 1)
+
+]
+
+
+def games_index(request):
+    return render(request, 'games/index.html', {'games': games})
