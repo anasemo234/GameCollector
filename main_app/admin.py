a6b10c118c9740cc59323e0c8b74408f46fb2290
Anasemos Kassahun <anasemosaka@gmail.com>
2022-07-07 00:10:06 -0400

    added review model and updated admin

--- main_app/admin.py
+++ main_app/admin.py
@@ -1,7 +1,10 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import Game
+from .models import Game, Review
 
 # Register your models here
 admin.site.register(Game)
+
+# register the new Review Model
+admin.site.register(Review)
--- main_app/models.py
+++ main_app/models.py
@@ -2,6 +2,11 @@ from django.db import models
 from django.urls import reverse
 
 # Create your models here.
+OPINIONS = (
+    ('L', 'Like'),
+    ('D', "Don't like"),
+    ('N', 'Never played')
+)
 
 
 class Game(models.Model):
@@ -18,5 +23,25 @@ class Game(models.Model):
 
 # Add this method
 
+
     def get_absolute_url(self):
         return reverse('detail', kwargs={'game_id': self.id})
+
+
+class Review(models.Model):
+    date = models.DateField('review date')
+    opinion = models.CharField(
+        max_length=1,
+        # add the 'choices' field option
+        choices=OPINIONS,
+        # set the default value for meal to be 'B'
+        default=OPINIONS[0][0]
+    )
+
+
+# Create a cat_id FK
+    game = models.ForeignKey(Game, on_delete=models.CASCADE)
+
+    def __str__(self):
+        # Nice method for obtaining the friendly value of a Field.choice
+        return f"{self.get_opinion_display()} on {self.date}"
